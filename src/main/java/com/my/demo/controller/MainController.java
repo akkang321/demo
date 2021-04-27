8ef9a7311081186b053461b812c3a60fe51c8fe9
ROG <ROG@DESKTOP-56MR0JA>
2021-04-27 22:27:00 +0900

    수정

---
+++ src/main/java/com/my/demo/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.my.demo.controller;
+
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value = { "/index", "/" })
+	public String index(@RequestParam Map<String, Object> param, HttpServletRequest request, Model model,  Locale locale) throws JsonMappingException, JsonProcessingException {
+		
+		return "index";
+	}
+	
+	
+}
